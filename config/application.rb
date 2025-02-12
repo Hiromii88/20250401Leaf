e538dacf1318de57aa4ef86b8446a84e19ce7ef8
Hiromii88 <goeson88@gmail.com>
2025-02-12 21:21:17 +0900

    画像挿入時のバグ修正

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Blog
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.active_record.legacy_connection_handling = false
+
 
     config.generators do |g|
       g.assets false
